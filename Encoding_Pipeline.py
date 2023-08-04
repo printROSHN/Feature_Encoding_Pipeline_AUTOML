2edcc3f22344ecc722c7df9d0ce0d1164c4ca79b
Roshan Pokal <roshukv02@gmail.com>
2023-08-04 12:30:46 +0530

    initial commit

--- Encoding_Pipeline.py
+++ Encoding_Pipeline.py
@@ -14,6 +14,7 @@ from category_encoders.cat_boost import CatBoostEncoder
 from category_encoders.james_stein import JamesSteinEncoder
 from category_encoders.one_hot import OneHotEncoder
 from category_encoders.leave_one_out import LeaveOneOutEncoder
+from category_encoders.binary import BinaryEncoder
 from sklearn.model_selection import StratifiedKFold, RepeatedStratifiedKFold
 from scipy.stats import spearmanr
 import scipy.sparse as sp
@@ -25,6 +26,9 @@ def get_single_encoder(encoder_name: str, cat_cols: list):
     :param cat_cols: Cat columns for encoding
     :return: Categorical encoder
     """
+    if encoder_name == "BinaryEncoder":
+        encoder = BinaryEncoder(cols=cat_cols)
+
     if encoder_name == "LeaveOneOutEncoder":
         encoder = LeaveOneOutEncoder(cols=cat_cols)
 
@@ -63,6 +67,9 @@ def get_single_encoder(encoder_name: str, cat_cols: list):
 
     if encoder_name == "MEstimateEncoder":
         encoder = MEstimateEncoder(cols=cat_cols)
+
+    if encoder_name == "OneHotEncoder":
+        encoder = OneHotEncoder(cols=cat_cols)
     return encoder
 
 def process_cols(data):
@@ -137,41 +144,43 @@ def is_interval_categorical_ordinal(data_column):
 
 def columns_with_low_cardinality(data, threshold=15):
     low_cardinality_columns = []
+    high_cardinality_columns = []
 
     for col in data.columns:
         unique_values = data[col].nunique()
         if unique_values < threshold:
             low_cardinality_columns.append(col)
+        else:
+            high_cardinality_columns.append(col)
 
-    return low_cardinality_columns
-
+    return low_cardinality_columns,high_cardinality_columns
 
 
 def estimate_sparse_matrix_memory_usage(columns, sample_data):
     memory_usages_mb = {}
     total_memory = 0
+
     # Calculate the number of rows and columns
     num_rows = len(sample_data)
     num_columns = len(columns)
 
-    # Convert the sample data to a sparse matrix
-    sparse_matrix = sp.csr_matrix(sample_data)
+    # Initialize a dictionary to store column indices and their respective non-zero element counts
+    col_indices_nonzero_elements = {col: 0 for col in columns}
 
+    # Iterate through the sample data to count non-zero elements for each column
     for col in columns:
-        # Get the column index based on the column name
         col_index = sample_data.columns.get_loc(col)
+        nonzero_elements = np.count_nonzero(sample_data.iloc[:, col_index])
+        col_indices_nonzero_elements[col] = nonzero_elements
 
-        # Get the number of non-zero elements for the column
-        num_nonzero_elements = len(np.unique(sparse_matrix.indices[sparse_matrix.indptr[col_index]:sparse_matrix.indptr[col_index + 1]]))
-
-        # Calculate the memory usage in bytes for the column
-        dtype_size = np.dtype(sparse_matrix.dtype).itemsize
+    # Calculate the memory usage for each column and the total memory usage
+    for col, num_nonzero_elements in col_indices_nonzero_elements.items():
+        dtype_size = np.dtype(sample_data.dtypes[col]).itemsize
         memory_usage_bytes = num_nonzero_elements * dtype_size
-
-        # Convert memory usage to a more human-readable format
         memory_usage_mb = memory_usage_bytes / (1024 * 1024)
         total_memory += memory_usage_mb
         memory_usages_mb[col] = memory_usage_mb
+
     print(memory_usages_mb)
     return total_memory
 
@@ -195,27 +204,45 @@ def estimate_sparse_matrix_memory_usage(columns, sample_data):
 #             1.Decision tree based algorithm <--
 #                 1. ---------------------------|------
 #                 2. One hot encoding           |     |
-#             2. lead to memory issues    -------     |
+#             2. lead to memory issues    <------     |
 #                 1. Is some loss acceptable<---------|
 #                     1.1. Binary encoding
 #                     1.2. Feature hashing encodings
 #                 2. Handle Overfitting
 #                     1. Leave One Out Encoder
 #                     2. TARGET, JAMES-STIEN ....
+#      3.Binary
+#      4.Merge Columns
 
 
+
+
+# The below code is performing feature encoding on a dataset. It reads a CSV file, separates the
+# target variable from the rest of the data, and then applies different encoding techniques to the
+# categorical variables. It identifies whether each categorical variable is nominal or ordinal based
+# on its correlation with the target variable. It then applies specific encoding techniques based on
+# the type of variable. Finally, it combines all the encoded variables with the numerical variables to
+# create a final encoded dataset.
 
 
 if __name__ == "__main__":
-    encoded_data_new = pd.DataFrame()
-    data = pd.read_csv("C:/CODE/JIO (JPL)/AutoML Feature Encoding/adult.csv")
+    merge_list = []
+    data = pd.read_csv("C:/CODE/JIO (JPL)/Feature_Encoding_Pipeline_AUTOML/data/adult.csv",sep=",")
+    print("\nData => \n",data.head())
+    print("\nColumn List : \n",data.columns.tolist())
 
-    target = input("Enter Target Column = ")
+    target = input("\nEnter Target Column = ")
 
     # Separate categorical and target variables
 
     non_target_data = data.drop(target, axis=1)
     target_col_data = data[target]
+
+    #TODO : Apply enconding to target to make it numeric
+    #^ Applying Ordianl Encoding
+    enc = get_single_encoder("OrdinalEncoder", target)
+    encoded_target_col = enc.fit_transform(target_col_data) #*
+    print("\nEncoded target: ", encoded_target_col)
 
     # Seperating / Removing NaN columns
     non_target_data = remove_high_missing_columns(non_target_data)
@@ -229,7 +256,7 @@ if __name__ == "__main__":
     nominal_col, ordinal_col, binary_col = [],[],[]
 
     # Output the result and seperate the columns
-    print("Categorical Column Information:")
+    print("\n\nCategorical Column Information:")
     for col, info in categorical_info.items():
         print(f"{col}: {info}")
         if(info == "Ordinal"):
@@ -245,11 +272,15 @@ if __name__ == "__main__":
     ordinal_data = data[ordinal_col].copy()
     nominal_data = data[nominal_col].copy()
     binary_data = data[binary_col].copy()
-    print(ordinal_data.head())
-    print(nominal_data.head())
-    print(binary_data.head())
+    # print(ordinal_data.head())
+    # print(nominal_data.head())
+    # print(binary_data.head())
+
     interval_ordinal_col = []
     non_interval_ordinal_col = []
+    encoded_ordinal_non_interval = []
+    encoded_ordinal_interval = []
+    
     #* TODO : CHECK FOR ORDINAL COLUMNS AND PROCESS IT
 
     for col in ordinal_data:
@@ -257,18 +288,23 @@ if __name__ == "__main__":
             interval_ordinal_col.append(col)
         else:
             non_interval_ordinal_col.append(col)
-    print("interval => \n",interval_ordinal_col)
-    print("non_interval => \n",non_interval_ordinal_col)
+    print("\nColumns with interval => \n",interval_ordinal_col)
+    print("\nColumns wit no_interval => \n",non_interval_ordinal_col)
 
     #* TODO: APPLY ENCODINGS ON THE INTERVAL DATA
     #^ Applying Backward difference encoder
-    enc = get_single_encoder("BackwardDifferenceEncoder", non_interval_ordinal_col)
-    encoded_ordinal_non_interval = enc.fit_transform(data[non_interval_ordinal_col]) #*
+    if(len(interval_ordinal_col) != 0):
+        enc = get_single_encoder("BackwardDifferenceEncoder", non_interval_ordinal_col)
+        encoded_ordinal_non_interval = enc.fit_transform(data[non_interval_ordinal_col],encoded_target_col) #*
+        print("\nBWD - ORDINAL_NON_INTERVAL ====> \n",encoded_ordinal_non_interval)  
+        merge_list.append(encoded_ordinal_non_interval)
 
     #^ Applying Ordianl Encoding
-    enc = get_single_encoder("OrdinalEncoder", interval_ordinal_col)
-    encoded_ordinal_interval = enc.fit_transform(data[interval_ordinal_col]) #*
-
+    if(len(non_interval_ordinal_col) != 0):
+        enc = get_single_encoder("OrdinalEncoder", interval_ordinal_col)
+        encoded_ordinal_interval = enc.fit_transform(data[interval_ordinal_col],encoded_target_col) #*
+        print("\nORDINAL - ORDINAL_INTERVAL ====> \n",encoded_ordinal_interval)
+        merge_list.append(encoded_ordinal_interval)  
 
     # TODO: Ask for Contrast Encoders
 
@@ -276,53 +312,86 @@ if __name__ == "__main__":
 
     # Checking the cardinality and seperating the columns
     #*  TODO: Pass column data
-    low_cardinality_col = columns_with_low_cardinality(nominal_data)
-    high_cardinality_col = nominal_data.drop(low_cardinality_col, axis=1)
+    low_cardinality_col, high_cardinality_col = columns_with_low_cardinality(nominal_data)
+    #high_cardinality_col = nominal_data.drop(low_cardinality_col, axis=1).columns.tolist()
 
+    print("\nlow cardinality columns = ", low_cardinality_col)
+    print("\nhigh cardinality columns = ", high_cardinality_col)
 
     #* TODO: Check for decision tree based algo
-    decision_tree_algo = input("Is the algorithm Tree Based (low cardinality) (yes/no) = ").lower()
-    if(decision_tree_algo == "no"):
-        # TODO: Apply One-Hot Encoding (low_cardinality)
-        pass
-    else:
-        handle_overfitting = input("Do you want to enter overfiting (low cardinality) (yes/no) = ").lower()
-        if(handle_overfitting == "yes"):
-            #* TODO: Apply Leave-one-out Encoding
-            enc = get_single_encoder("LeaveOneOutEncoder", interval_ordinal_col)
-            encoded_low_cardinality = enc.fit_transform(data[interval_ordinal_col],target_col_data) #*
-            
+    if(len(low_cardinality_col) != 0):
+        decision_tree_algo = input("\nIs the algorithm Tree Based (low cardinality) (yes/no) = ").lower()
+        if(decision_tree_algo == "no"):
+            # TODO: Apply One-Hot Encoding (low_cardinality)
+            enc = get_single_encoder("OneHotEncoder", low_cardinality_col)
+            encoded_low_cardinality = enc.fit_transform(data[low_cardinality_col],encoded_target_col) #*
+            print("\n\nONE HOT Encoding - Low cardinality ====> \n",encoded_low_cardinality) 
+
         else:
-            #* TODO : Apply james-stien, target etc..
-            enc = get_single_encoder("JamesSteinEncoder", interval_ordinal_col)
-            encoded_low_cardinality = enc.fit_transform(data.iloc[:, interval_ordinal_col]) #*
-                
-            
-    
+            handle_overfitting = input("\nDo you want to enter overfiting (low cardinality) (yes/no) = ").lower()
+            if(handle_overfitting == "yes"):
+                #* TODO: Apply Leave-one-out Encoding
+                enc = get_single_encoder("LeaveOneOutEncoder", low_cardinality_col)
+                encoded_low_cardinality = enc.fit_transform(data[low_cardinality_col],encoded_target_col) #*
+                print("\nLEAVE ONE OUT - Low cardinality ====> \n",encoded_low_cardinality)  
+            else:
+                #* TODO : Apply james-stien, target etc..
+                enc = get_single_encoder("TargetEncoder", low_cardinality_col)
+                encoded_low_cardinality = enc.fit_transform(data[low_cardinality_col],encoded_target_col) #*
+                print("\nTARGET - Low cardinality ====> \n",encoded_low_cardinality)
+        merge_list.append(encoded_low_cardinality)
+        
     #& Processing High Cardinality data
-    memory_check = estimate_sparse_matrix_memory_usage(high_cardinality_col, nominal_data)
-    if(memory_check > 10):
-        handle_overfitting = input("Do you want to enter overfiting (high cardinality) (yes/no) = ").lower()
-        if(handle_overfitting == "yes"):
-            #* TODO: Apply Leave-one-out Encoding
-            enc = get_single_encoder("LeaveOneOutEncoder", interval_ordinal_col)
-            encoded_high_cardinality = enc.fit_transform(data[interval_ordinal_col], target_col_data) #*
+    if(len(high_cardinality_col) != 0):
+        memory_check = estimate_sparse_matrix_memory_usage(high_cardinality_col, nominal_data[high_cardinality_col])
+        print("\n\nCalculating estimate sparse matrix memory usage....... ")
+        print("\n\nMEMORY USED = ", memory_check)
+        if(memory_check > 10):
+            handle_overfitting = input("\nDo you want to enter overfiting (high cardinality) (yes/no) = ").lower()
+            if(handle_overfitting == "yes"):
+                #* TODO: Apply Leave-one-out Encoding
+                enc = get_single_encoder("LeaveOneOutEncoder", high_cardinality_col)
+                encoded_high_cardinality = enc.fit_transform(data[high_cardinality_col], encoded_target_col) #*
+                print("\nLEAVE ONE OUT - high cardinality ====> \n",encoded_high_cardinality)
+            else:
+                #* TODO : Apply james-stien, target etc..
+                enc = get_single_encoder("JamesSteinEncoder", high_cardinality_col)
+                encoded_high_cardinality = enc.fit_transform(data[high_cardinality_col],encoded_target_col) #*
+                print("\nJAMES-STIEN - high cardinality ====> \n",encoded_high_cardinality)
         else:
-            #* TODO : Apply james-stien, target etc..
-            enc = get_single_encoder("JamesSteinEncoder", interval_ordinal_col)
-            encoded_high_cardinality = enc.fit_transform(data[interval_ordinal_col]) #*
+            decision_tree_algo = input("\nIs the algorithm Tree Based (high cardinality) (yes/no) = ").lower()
+            if(decision_tree_algo == "no"):
+                # TODO: Apply One-Hot Encoding (high_cardinality)
+                enc = get_single_encoder("OneHotEncoder", high_cardinality_col)
+                encoded_high_cardinality = enc.fit_transform(data[high_cardinality_col],encoded_target_col) #*
+                print("\n\nONE HOT Encoding - High cardinality ====> \n",encoded_high_cardinality) 
+                pass
+            else:
+                handle_overfitting = input("\nDo you want to enter overfiting (low cardinality) (yes/no) = ").lower()
+                if(handle_overfitting == "yes"):
+                    #* TODO: Apply Leave-one-out Encoding
+                    enc = get_single_encoder("LeaveOneOutEncoder", high_cardinality_col)
+                    encoded_high_cardinality = enc.fit_transform(data[high_cardinality_col],encoded_target_col) #*
+                    print("\nLEAVE ONE OUT - High cardinality ====> \n",encoded_high_cardinality)  
+                else:
+                    #* TODO : Apply james-stien, target etc..
+                    enc = get_single_encoder("TargetEncoder", high_cardinality_col)
+                    encoded_high_cardinality = enc.fit_transform(data[high_cardinality_col],encoded_target_col) #*
+                    print("\nTARGET ENC - High cardinality ====> \n",encoded_high_cardinality)
             
+            merge_list.append(encoded_high_cardinality)
+        
+    #* Applying Binary encoding to Binary Columns
+    if(len(binary_col) != 0):
+        enc = get_single_encoder("BinaryEncoder", binary_col)
+        encoded_binary_columns = enc.fit_transform(data[binary_col],encoded_target_col) #*
+        merge_list.append(encoded_binary_columns)
 
-    
+
     #& Combine all data for final output
-    result_df = pd.concat([encoded_ordinal_interval, encoded_ordinal_non_interval,encoded_high_cardinality,num_col], axis=1)
-    print(result_df)
-
-
-
-
-
-    # TODO : CREATE A DATA FOLDER TO STORE DATA , FOR QUICK SWAP
+    merge_list.append(num_col)
+    result_df = pd.concat(merge_list, axis=1)
+    print("\n\nFinal Encoded DataFrame = \n\n",result_df)
 
 
 
